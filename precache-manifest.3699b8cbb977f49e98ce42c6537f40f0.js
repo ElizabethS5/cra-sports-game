69f5c46fd3dc0603d0fdc94e5fe7b773d459377c
Elizabeth5 <escheidt5@gmail.com>
2020-08-04 16:09:52 -0400

    Updates

---
+++ precache-manifest.3699b8cbb977f49e98ce42c6537f40f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b013a233e2a1038a090733ca8f28116",
+    "url": "/cra-sports-game/index.html"
+  },
+  {
+    "revision": "7de47615126f89e1fe37",
+    "url": "/cra-sports-game/static/css/main.7d5ef757.chunk.css"
+  },
+  {
+    "revision": "717a59e0a001c6205903",
+    "url": "/cra-sports-game/static/js/2.ceab36eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra-sports-game/static/js/2.ceab36eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de47615126f89e1fe37",
+    "url": "/cra-sports-game/static/js/main.cfc95fb3.chunk.js"
+  },
+  {
+    "revision": "3489b545a4728626e8f6",
+    "url": "/cra-sports-game/static/js/runtime-main.edc2cdfd.js"
+  },
+  {
+    "revision": "f4afced42902a14c1a446832f27a08f2",
+    "url": "/cra-sports-game/static/media/bunny.f4afced4.png"
+  },
+  {
+    "revision": "15752ad88483d7509c9d94b77fe3ffb1",
+    "url": "/cra-sports-game/static/media/hound.15752ad8.png"
+  },
+  {
+    "revision": "b172ee0d881b819f20db857af14192d1",
+    "url": "/cra-sports-game/static/media/raccoon.b172ee0d.png"
+  },
+  {
+    "revision": "872073a1918e0ac80adf92bdfd2fa67b",
+    "url": "/cra-sports-game/static/media/smb_1-up.872073a1.wav"
+  },
+  {
+    "revision": "f0fb24efc66d05e0f7a31ce5490cf288",
+    "url": "/cra-sports-game/static/media/smb_fireball.f0fb24ef.wav"
+  },
+  {
+    "revision": "87db0b851957568987b1b16cab177dbe",
+    "url": "/cra-sports-game/static/media/squirrel.87db0b85.png"
+  }
+]);
